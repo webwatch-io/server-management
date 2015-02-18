092655e2ce5b6da521ba9ce5685a486b970184cf
mwagstaff <mike.wagstaff@gmail.com>
2015-02-18 23:00:05 +0000

    test

--- setup-server-ubuntu.sh
+++ setup-server-ubuntu.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+# bash <(curl -s https://raw.githubusercontent.com/webwatch-io/server-management/master/setup-server-ubuntu.sh)
+echo hi!
+sudo apt-get update && sudo apt-get -y upgrade
