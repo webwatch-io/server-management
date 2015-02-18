b38c9c3b34f3c4209c1fec2198c5b36c987dfaaf
mwagstaff <mike.wagstaff@gmail.com>
2015-02-18 23:11:08 +0000

    fleshing out script

--- setup-server-ubuntu.sh
+++ setup-server-ubuntu.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 # bash <(curl -s https://raw.githubusercontent.com/webwatch-io/server-management/master/setup-server-ubuntu.sh)
-echo hi!
-sudo apt-get update && sudo apt-get -y upgrade
+
+
+#
+# Begin supporting functions
+#
+
+function applyPatches {
+  echo Applying OS patches...
+  sudo apt-get update && sudo apt-get -y upgrade
+}
+
+function addPublicKey {
+  echo Adding public key: $1.pub
+  curl -s https://raw.githubusercontent.com/webwatch-io/server-management/master/keys/$1.pub >> ~/.ssh/authorized_keys
+}
+
+function main {
+  applyPatches
+  addPublicKey workstation_nimh.pub
+}
+
+
+#
+# Begin main body of script
+#
+
+main
